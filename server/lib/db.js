888a78618d7bc0e3708bd54d00c7db5665fb8168
Naveed Sayyed <sayyednaveed107@gmail.com>
2025-11-05 13:44:51 +0530

    chat app imagefix

--- server/lib/db.js
+++ server/lib/db.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 // Function to connect to the mongodb database
 export const connectDB = async () => {
     try {
+        console.log(process.env.MONGODB_URI)
         mongoose.connection.on('connected', () => console.log('Database Connected'));
         await mongoose.connect(`${process.env.MONGODB_URI}/chat-app`);
     } catch (error) {
